8a9c1907658229c0c029c81aee5948f3206e6a0c
pjy1368 <56083021+pjy1368@users.noreply.github.com>
2020-03-09 20:31:38 +0900

    Create 15969.java

---
+++ 15969.java
@@ -0,0 +1,29 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+		int N = Integer.parseInt(br.readLine());
+
+		int[] arr = new int[N];
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		for (int i = 0; i < N; i++) {
+			arr[i] = Integer.parseInt(st.nextToken());
+		}
+		Arrays.sort(arr);
+		
+		bw.write((arr[N - 1] - arr[0]) + "\n");
+		bw.flush();
+		bw.close();
+		br.close();
+	}
+
+}
