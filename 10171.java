0816b15c7dd420975d0ff9b070876376caf106b1
pjy1368 <56083021+pjy1368@users.noreply.github.com>
2020-03-09 20:07:07 +0900

    Create 10171.java

---
+++ 10171.java
@@ -0,0 +1,21 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+public class Main {
+
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		
+		bw.write("\\    /\\\n");
+		bw.write(" )  ( ')\n");
+		bw.write("(  /  )\n");
+		bw.write(" \\(__)|\n");
+		bw.flush();
+		bw.close();
+		br.close();
+	}
+
+}
