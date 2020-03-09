974e95f120451b3a66af0be7745de58645320a2d
pjy1368 <56083021+pjy1368@users.noreply.github.com>
2020-03-09 18:17:39 +0900

    Create 2609.java

---
+++ 2609.java
@@ -0,0 +1,36 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+
+public class Main {
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		// TODO Auto-generated method stub
+		BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		String input = bf.readLine();
+		StringTokenizer st = new StringTokenizer(input);
+		int A = Integer.parseInt(st.nextToken());
+		int B = Integer.parseInt(st.nextToken());
+		
+		bw.write(GCD(A, B) + "\n");
+		bw.write(LCM(A, B) + "\n");
+		bw.flush();
+		bf.close();
+		bw.close();
+	}
+	
+	public static int GCD(int A, int B) {
+		if(B == 0) {
+			return A;
+		}
+		return GCD(B, A % B);
+	}
+	
+	public static int LCM(int A, int B) {
+		return A * B / GCD(A, B);
+	}
+
+}
