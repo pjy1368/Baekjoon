ef70ebd747d4db63a89a89845c06d46a8fc73450
pjy1368@konkuk.ac.kr <pjy1368@konkuk.ac.kr>
2020-04-14 12:36:37 +0900

    com

--- 1000~9999/1912.java
+++ 1000~9999/1912.java
@@ -1,46 +1,38 @@
+package noj.am;
+
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
-import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 import java.util.StringTokenizer;
 
 public class Main {
-	public static void main(String[] args) throws NumberFormatException, IOException {
-		// TODO Auto-generated method stub
-		BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-		int N = Integer.parseInt(bf.readLine());
-		long[] sequence = new long[100001]; // 수열
-		long[] dp = new long[100001]; // 부분합
 
-		StringTokenizer st = new StringTokenizer(bf.readLine());
-		for (int i = 1; i <= N; i++) {
-			int temp = Integer.parseInt(st.nextToken());
-			sequence[i] = temp;
+		int N = Integer.parseInt(br.readLine());
+
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		long[] dp = new long[N];
+		for (int i = 0; i < N; i++) {
+			dp[i] = Integer.parseInt(st.nextToken());
 		}
 
-		long sum = 0;
-		long num = 0;
-
-		for (int i = 1; i <= N; i++) {
-			num = sum = sequence[i];
-			for (int j = i + 1; j <= N; j++) {
-				num += sequence[j];
-				sum = Math.max(sum, num);
+		long max = dp[0];
+		for (int i = 1; i < N; i++) {
+			if (dp[i - 1] > 0 && dp[i] + dp[i - 1] > 0) {
+				dp[i] += dp[i - 1];
 			}
-			dp[i] = sum;
+
+			max = Math.max(max, dp[i]);
 		}
 
-		long ans = Integer.MIN_VALUE;
-		for (int i = 1; i <= N; i++) {
-			ans = Math.max(ans, dp[i]);
-		}
-
-		bw.write(ans + "\n");
+		bw.write(max + "\n");
 		bw.flush();
 		bw.close();
-		bf.close();
-	}
+		br.close();
 
+	}
 }
