017bb15f9a8fda789a99604f09548031be23a7ab
pjy1368@konkuk.ac.kr <pjy1368@konkuk.ac.kr>
2020-05-11 22:51:22 +0900

    com

---
+++ 1000~9999/1082.java
@@ -0,0 +1,21 @@
+package noj.am;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+
+public class Main {
+
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		StringTokenizer st;
+
+		bw.flush();
+		bw.close();
+		br.close();
+	}
+
+}
