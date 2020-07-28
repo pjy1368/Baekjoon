14d24001e1066e579d0b0db681743552e1004a6e
pjy1368@konkuk.ac.kr <pjy1368@konkuk.ac.kr>
2020-07-28 21:55:02 +0900

    com

---
+++ 1000~9999/2556.java
@@ -0,0 +1,28 @@
+package no.jam1;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+public class Main {
+
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		
+		int N = Integer.parseInt(br.readLine());
+		for(int i = 1; i <= N; i++) {
+			for(int j = 1; j <= N; j++) {
+				bw.write("*");
+			}
+			bw.write("\n");
+		}
+		
+		bw.flush();
+		bw.close();
+		br.close();
+	}
+
+}
