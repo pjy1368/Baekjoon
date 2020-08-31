187ac24a526b76109a6db023cfd10895dab8745e
pjy1368@konkuk.ac.kr <pjy1368@konkuk.ac.kr>
2020-08-31 23:07:21 +0900

    com

--- 10000~/11872.java
+++ 10000~/11872.java
@@ -13,29 +13,19 @@ public class Main {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 		StringTokenizer st;
-		System.out.println(Math.sqrt(2562991876L));
+
 		int N = Integer.parseInt(br.readLine());
 
-		long result = 0;
+		int result = 0;
 
 		st = new StringTokenizer(br.readLine());
 		for (int i = 0; i < N; i++) {
-			long temp = Long.parseLong(st.nextToken());
+			int temp = Integer.parseInt(st.nextToken());
 
-			if (temp >= 1 && temp < 4) {
-				temp = 0;
-			} else if (temp >= 4 && temp < 16) {
-				temp = 1;
-			} else if (temp >= 16 && temp < 82) {
-				temp = 2;
-			} else if (temp >= 82 && temp < 6724) {
-				temp = 0;
-			} else if (temp >= 6724 && temp < 50626) {
-				temp = 3;
-			} else if (temp >= 50626 && temp < 2562991876L) {
-				temp = 1;
-			} else {
-				temp = 2;
+			if (temp % 4 == 0) {
+				temp--;
+			} else if (temp % 4 == 3) {
+				temp++;
 			}
 
 			result ^= temp;
